1c6d82a7833898a1b67e88c9469990626fc27236
mnl187 <mnl187@gmail.com>
2022-11-02 06:34:36 +0100

    dodano funkcję zmiany ilości stolików

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ let tablesCount = 25;
 megaRestaurant
     .on('open', () => console.log('Otwarto restaurację.'))
     .on('close', () => console.log('Zamknieto restaurację.'))
+    .on('tableCountUpdate', change => {
+        tablesCount += change;
+        console.log(`Dostępnych stolików ${tablesCount}.`);
+    });
 
 megaRestaurant.open();
+
+
+
+
 megaRestaurant.close();
