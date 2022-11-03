81b17dd3eeb2863d96bb8cce380f3181a63cbf03
mnl187 <mnl187@gmail.com>
2022-11-03 06:41:01 +0100

    ts

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+import { Restaurant } from "./restaurant";
+const megaRestaurant = new Restaurant();
+let tablesCount = 25;
+megaRestaurant
+    .on('open', () => console.log('Otwarto restaurację.'))
+    .on('close', () => console.log('Zamknieto restaurację.'))
+    .on('tableCountUpdate', change => {
+    tablesCount += change;
+    console.log(`Dostępnych stolików ${tablesCount}.`);
+});
+megaRestaurant.open();
+megaRestaurant.takeTableWithoutReservation();
+megaRestaurant.takeTableWithoutReservation();
+megaRestaurant.reserveTable();
+megaRestaurant.cancelTableReservation();
+megaRestaurant.reserveTable();
+megaRestaurant.reserveTable();
+megaRestaurant.takeTableWithoutReservation();
+megaRestaurant.takeTableWithoutReservation();
+megaRestaurant.cleanupTable();
+megaRestaurant.close();
+//# sourceMappingURL=index.js.map
---
+++ dist/restaurant.js
@@ -0,0 +1,28 @@
+import { EventEmitter } from "events";
+export class Restaurant extends EventEmitter {
+    // Otwarcie restauracji
+    open() {
+        this.emit('open');
+    }
+    // Zamknięcie restauracji
+    close() {
+        this.emit('close');
+    }
+    // Stolik został zarezerwowany na teraz
+    reserveTable() {
+        this.emit('tableCountUpdate', -1);
+    }
+    // Odwołano rezerwcję na stolik
+    cancelTableReservation() {
+        this.emit('tableCountUpdate', 1);
+    }
+    // Wzięto stolik bez rezerwacji
+    takeTableWithoutReservation() {
+        this.emit('tableCountUpdate', -1);
+    }
+    // Stolik po wyczyszczeniui wraca do użytku
+    cleanupTable() {
+        this.emit('tableCountUpdate', 1);
+    }
+}
+//# sourceMappingURL=restaurant.js.map
